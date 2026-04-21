bfc45aadc761323231ffed6184f74fac2d315241
usti-ks <ksenia.usus@gmail.com>
2026-04-21 20:35:07 +0500

    add safe gigachat config

---
+++ gigachat.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  port: 8787,
+  authUrl: 'https://ngw.devices.sberbank.ru:9443/api/v2/oauth',
+  apiBaseUrl: 'https://gigachat.devices.sberbank.ru/api/v1',
+  authKey: '',
+  scope: 'GIGACHAT_API_PERS',
+  model: 'GigaChat',
+};
